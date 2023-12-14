f7001635a39a680f73bbeb18cdbc078865603eb8
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-12-14 00:10:33 -0700

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+run_vm.sh \
+  -serial null \
+  -daemonize
+
+wait_ssh.sh
+
+rsync -avr \
+  --exclude ".git" \
+  --filter=':- .gitignore' \
+  /src \
+  root@localhost:/src
+
+ssh root@localhost 'bash -l -c /opt/bin/build.sh'
+
+rsync -avr root@localhost:/src /src
+
+save_vm.sh
