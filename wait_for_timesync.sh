614442cd2e3b56eecfd50de4171ae37f306794b7
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2024-03-04 14:13:12 -0700

    Force time to sync

--- wait_for_timesync.sh
+++ wait_for_timesync.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-echo "Waiting for timesync"
-while [[ "$(timedatectl status | grep 'System clock synchronized' | awk '{print $4}')" != "yes" ]]; do sleep 1; done
+if [[ "$(timedatectl status | grep 'NTP service:' | awk '{print $3}')" == "active" ]];then
+  echo "Waiting for timesync"
+  timedatectl set-ntp no || true
+  timedatectl set-ntp yes;
+  while [[ "$(timedatectl status | grep 'System clock synchronized' | awk '{print $4}')" != "yes" ]]; do
+    sleep 1;
+  done;
+fi;
